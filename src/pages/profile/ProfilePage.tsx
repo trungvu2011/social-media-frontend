84607c876987c062bf770c121d50b21768872b05
NXTruong <truongnguyen31032004@gmail.com>
2026-01-04 22:14:44 +0700

    fix error

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, use } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import Layout from "../../components/layout/Layout";
 import Post from "../../components/feed/Post";
